6d2236ac1965f4480678a0332cdbd34acb100519
Momen Helmy <momen.helmy.m@gmail.com>
2024-11-10 03:35:39 +0200

    Style the root layout

--- src/Layouts/RootLayout.jsx
+++ src/Layouts/RootLayout.jsx
@@ -1,16 +1,17 @@
 import { Link, Outlet } from "react-router-dom";
 import { BiMessageSquareDots } from "react-icons/bi";
-
+import "./layout.css";
 function Layout() {
   return (
-    <div>
+    <div className="rootLayout">
       <nav>
-        <Link to="/">
+        <Link to="/" className="logo">
           <span>
             <BiMessageSquareDots />
           </span>
           <span>FRYNDLY</span>
         </Link>
+        <div>User Info</div>
       </nav>
       <main>
         <Outlet />
