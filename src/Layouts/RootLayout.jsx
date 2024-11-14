31d94dfa1dcc20167ecda5ca385bcef35d72e107
Momen Helmy <momen.helmy.m@gmail.com>
2024-11-14 02:58:35 +0200

    Apply tailwind css and design the homepage

--- src/Layouts/RootLayout.jsx
+++ src/Layouts/RootLayout.jsx
@@ -18,26 +18,30 @@ if (!PUBLISHABLE_KEY) {
 function Layout() {
   return (
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <div className="rootLayout">
-        <nav>
-          <Link to="/" className="logo">
-            <span>
-              <BiMessageSquareDots />
-            </span>
-            <span>FRYNDLY</span>
-          </Link>
-          <div>
-            <SignedOut>
-              <SignInButton className="signInButton" mode="modal" />
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
-          </div>
-        </nav>
-        <main>
-          <Outlet />
-        </main>
+      <div className="h-[50rem] w-full dark:bg-black bg-white  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center">
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+
+        <div className="rootLayout z-20">
+          <nav className="flex items-center justify-between">
+            <Link to="/" className="flex items-center gap-2 text-2xl">
+              <span className="text-purple-500">
+                <BiMessageSquareDots />
+              </span>
+              <span>FRYNDLY</span>
+            </Link>
+            <div>
+              <SignedOut>
+                <SignInButton className="signInButton" mode="modal" />
+              </SignedOut>
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+            </div>
+          </nav>
+          <main className="flex-1 overflow-hidden">
+            <Outlet />
+          </main>
+        </div>
       </div>
     </ClerkProvider>
   );
--- src/Routes/Homepage.jsx
+++ src/Routes/Homepage.jsx
@@ -1,9 +1,25 @@
-import { Link } from "react-router-dom";
-
 function Homepage() {
   return (
-    <div>
-      <Link to="/dashboard">Dashboard</Link>
+    <div className="flex justify-between gap-2 items-center h-screen">
+      <div className="flex-1 flex flex-col justify-center">
+        <h1 className="my-2 font-semibold text-6xl lg:text-8xl uppercase font-fryndly bg-gradient-to-r from-purple-500 via-pink-300 to-blue-400 inline-block text-transparent bg-clip-text">
+          Fryndly ai
+        </h1>
+        <h2 className="capitalize font-fryndly text-3xl">
+          Your new ai friend that can chat, help and assist you to product{" "}
+        </h2>
+        <p className="text-sm text-neutral-400 mt-3">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi vel
+          necessitatibus itaque nam, dignissimos asperiores voluptates? Modi,
+          quisquam, qui in earum ducimus facilis id a soluta magni, et officiis
+          velit.
+        </p>
+        <button className="capitalize rounded-full bg-purple-600 text-white py-2 px-4 w-1/3 my-10">
+          Let&apos;s chat
+        </button>
+      </div>
+
+      <div className="flex-1">{/* the right section of the homepage */}</div>
     </div>
   );
 }
